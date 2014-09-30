21d198cc2cac06b5f408f677c7ac6a02dede77fb
Trygve Lie <github@trygve-lie.com>
2014-09-30 08:36:48 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,433 @@
-dom-utils
-=========
+# dom-utils
 
-Collection of misc DOM manipulation functions not natively provided by the DOM api's.
+Collection of misc DOM manipulation functions. These are helper
+functions which are not nativly provided by the DOM api's.
+
+
+
+## Installation
+
+```bash
+$ npm install dom-utils
+````
+
+
+
+## Tests
+
+```bash
+$ npm test
+````
+
+Tests are written in [mocha](http://visionmedia.github.io/mocha/) and run
+in node.js with the help of [jsdom](https://github.com/tmpvar/jsdom).
+
+
+
+## Basic usage
+
+Example on converting all values on an attribute to an `Array`.
+
+HTML:
+```html
+<div id="foo" class="foo bar"></div>
+```
+
+JS:
+```js
+var dom = require('dom-utils');
+
+var element = document.getElementById('foo');
+var attributes = dom.attributeToArray(element, 'class');
+
+console.log(attributes);  // returns ['foo', 'bar']
+```
+
+
+
+## Chaining
+
+Some functions can be chained. Iow; this is possible:
+
+```js
+dom.addFirstChildElement(root, newEl)
+   .removeLastChildElement(root);
+```
+
+
+
+## API
+
+The following methods are available:
+
+
+### .walkDown(node, callback)
+
+Walk the DOM downwards from a given 'node' and executes a
+callback on each `node`.
+
+The callback must return `true` for the walking to continue
+through the DOM. The walk is terminated by letting the
+callback return `false`.
+
+The current `node` which the callback is operating on is 
+available as `this` in the callback. The first variable to
+the callback is the `depth` of the node in the DOM tree.
+
+HTML:
+```html
+<section>
+    <div id="bar">
+        <p></p>
+        <p>
+            <span></span>
+        </p>
+    </div>
+</section>
+```
+
+JS:
+```js
+var dom = require('dom-utils');
+
+var element = document.getElementById('bar');
+dom.walkDown(element, function(depth){
+    if (this.nodeName === 'SPAN') {
+        console.log('Found a <span> at level: ' + depth);
+        return false;
+    } else {
+        return true;
+    }
+});
+```
+
+
+### .walkUp(node, callback)
+
+Walk the DOM upwards from a given 'node' and executes a
+callback on each `node`.
+
+The callback must return `true` for the walking to continue
+through the DOM. The walk is terminated by letting the
+callback return `false`.
+
+The current `node` which the callback is operating on is 
+available as `this` in the callback.
+
+HTML:
+```html
+<section>
+    <div>
+        <p></p>
+        <p>
+            <span id="bar"></span>
+        </p>
+    </div>
+</section>
+```
+
+JS:
+```js
+var dom = require('dom-utils');
+
+var element = document.getElementById('bar');
+dom.walkUp(element, function(){
+    if (this.nodeName === 'DIV') {
+        console.log('Found a <div>');
+        return false;
+    } else {
+        return true;
+    }
+});
+```
+
+
+### .attributeToArray(element, attribute)
+
+Parses the values of a attribute on a DOM element to a `Array`.
+
+HTML:
+```html
+<div id="foo" class="foo bar"></div>
+```
+
+JS:
+```js
+var dom = require('dom-utils');
+
+var element = document.getElementById('foo');
+var attributes = dom.attributeToArray(element, 'class');
+
+console.log(attributes);  // returns ['foo', 'bar']
+```
+
+
+### .removeChildElements(element)
+
+Removes all child elements of a DOM node.
+
+HTML before:
+```html
+<div id="foo">
+    <p>foo</p>
+    <p>bar</p>
+</div>
+```
+
+JS:
+```js
+var dom = require('dom-utils');
+
+var element = document.getElementById('foo');
+dom.removeChildElements(element);
+```
+
+HTML after:
+```html
+<div id="foo"></div>
+```
+
+Function is chainable.
+
+
+### .appendMultipleChildElements(root, elements)
+
+Appends a Array of elements as children to a root element.
+
+HTML before:
+```html
+<ul id="foo"></ul>
+```
+
+JS:
+```js
+var dom = require('dom-utils');
+
+var root = document.getElementById('foo');
+var children = [];
+
+children.push(document.createElement('li'));
+children.push(document.createElement('li'));
+
+dom.appendMultipleChildElements(root, children);
+```
+
+HTML after:
+```html
+<ul id="foo">
+    <li></li>
+    <li></li>
+</ul>
+```
+
+Function is chainable.
+
+
+### .addFirstChildElement(root, element)
+
+Appends an element as the first child to a root element.
+
+HTML before:
+```html
+<ul id="foo">
+    <li>a</li>
+    <li>b</li>
+</ul>
+```
+
+JS:
+```js
+var dom = require('dom-utils');
+
+var root = document.getElementById('foo');
+var element = document.createElement('li');
+element.innerText = 'x';
+
+dom.addFirstChildElement(root, element);
+```
+
+HTML after:
+```html
+<ul id="foo">
+    <li>x</li>
+    <li>a</li>
+    <li>b</li>
+</ul>
+```
+
+Function is chainable.
+
+
+### .removeLastChildElement(root)
+
+Removes the last child element from a root element.
+
+HTML before:
+```html
+<ul id="foo">
+    <li>a</li>
+    <li>b</li>
+    <li>c</li>
+</ul>
+```
+
+JS:
+```js
+var dom = require('dom-utils');
+
+var root = document.getElementById('foo');
+dom.removeLastChildElement(root);
+```
+
+HTML after:
+```html
+<ul id="foo">
+    <li>a</li>
+    <li>b</li>
+</ul>
+```
+
+Function is chainable.
+
+
+### .addSiblingAboveElement(root, element)
+
+Appends an element as a sibling above an element.
+
+HTML before:
+```html
+<div id="foo"></div>
+```
+
+JS:
+```js
+var dom = require('dom-utils');
+
+var root = document.getElementById('foo');
+var element = document.createElement('p');
+
+dom.addSiblingAboveElement(root, element);
+```
+
+HTML after:
+```html
+<p></p>
+<div id="foo"></div>
+```
+
+Function is chainable.
+
+
+### .addSiblingBelowElement(root, element)
+
+Appends an element as a sibling below an element.
+
+HTML before:
+```html
+<div id="foo"></div>
+```
+
+JS:
+```js
+var dom = require('dom-utils');
+
+var root = document.getElementById('foo');
+var element = document.createElement('p');
+
+dom.addSiblingBelowElement(root, element);
+```
+
+HTML after:
+```html
+<div id="foo"></div>
+<p></p>
+```
+
+Function is chainable.
+
+
+### .replaceElement(existingElement, newElement)
+
+Replace an element (and all its children) with a new element
+and returns the new element.
+
+HTML before:
+```html
+<div id="foo">
+    <p></p>
+</div>
+```
+
+JS:
+```js
+var dom = require('dom-utils');
+
+var existing = document.getElementById('foo');
+var element = document.createElement('section');
+
+dom.replaceElement(existing, element);
+```
+
+HTML after:
+```html
+<section></section>
+```
+
+
+### .removeElement(element)
+
+Remove a given element (and all its children).
+
+HTML before:
+```html
+<div>
+    <p id="foo"></p>
+</div>
+```
+
+JS:
+```js
+var dom = require('dom-utils');
+
+var element = document.getElementById('foo');
+dom.removeElement(element);
+```
+
+HTML after:
+```html
+<div></div>
+```
+
+Function is chainable.
+
+
+
+## Environments
+
+Browser and node.js with [jsdom](https://github.com/tmpvar/jsdom).
+
+
+
+## License 
+
+The MIT License (MIT)
+
+Copyright (c) 2014 - Trygve Lie - post@trygve-lie.com
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
---
+++ test/dom.utils.js
@@ -0,0 +1,479 @@
+/* jshint node: true, strict: true */
+/* global describe: true, it: true, before: true */
+
+"use strict";
+
+var mocha           = require('mocha'),
+    assert          = require('chai').assert,
+    jsdom           = require('jsdom'),
+    utils           = require('../');
+
+
+
+describe('dom.utils.attributeToArray()', function(){
+
+    describe('class="foo bar xyz"', function(){
+        
+        before(function(){
+            var fragment    = jsdom.jsdom('<div id="root" class="foo bar xyz">text</div>', jsdom.level(3, 'core'));
+            this.result     = utils.attributeToArray(fragment.getElementById('root'), 'class');
+        });
+
+        it('should return an array', function(){
+            assert.isArray(this.result);
+        });
+
+        it('should return an array with the length of 3', function(){
+            assert.equal(3, this.result.length);
+        });
+
+        it('should return an array with "foo" at index 0', function(){
+            assert.equal("foo", this.result[0]);
+        });
+
+        it('should return an array with "bar" at index 1', function(){
+            assert.equal("bar", this.result[1]);
+        });
+
+        it('should return an array with "xyz" at index 2', function(){
+            assert.equal("xyz", this.result[2]);
+        });
+
+    });
+
+});
+
+
+
+describe('dom.utils.removeChildElements()', function(){
+
+    describe('element has child elements', function(){
+
+        it('should remove all child elements', function(done){
+            var result = '<section id="root"></section>';
+
+            jsdom.env('<section id="root"><div><p>text</p></div><div></div><div></div></section>', function(error, window){
+                var root = window.document.getElementById('root');
+                utils.removeChildElements(window.document.getElementById('root'));
+                assert.equal(result, root.outerHTML);
+                done();
+            });
+
+        });
+
+    });
+
+    describe('element has no child elements', function(){
+
+        it('should keep the root untouched', function(done){
+            var result = '<section id="root"></section>';
+
+            jsdom.env('<section id="root"></section>', function(error, window){
+                var root = window.document.getElementById('root');
+                utils.removeChildElements(window.document.getElementById('root'));
+                assert.equal(result, root.outerHTML);
+                done();
+            });
+
+        });
+
+    });
+
+    describe('function is chainable', function(){
+
+        it('should return "this"', function(done){
+
+            jsdom.env('<section id="root"><div><p>text</p></div><div></div><div></div></section>', function(error, window){
+                var root = window.document.getElementById('root');
+                var result = utils.removeChildElements(window.document.getElementById('root'));
+                assert.equal(result, utils);
+                done();
+            });
+
+        });
+
+    });
+
+});
+
+
+
+describe('dom.utils.appendMultipleChildElements()', function(){
+
+    describe('append an empty Array', function(){
+
+        it('should preserve "root" as before appended', function(done){
+            var result = '<section id="root"></section>';
+
+            jsdom.env('<section id="root"></section>', function(error, window){
+                var doc         = window.document,
+                    root        = doc.getElementById('root'),
+                    elements    = [];
+
+                utils.appendMultipleChildElements(root, elements);
+
+                assert.equal(result, root.outerHTML);
+                done();
+            });
+
+        });
+
+    });
+
+    describe('append an Array with 3 individaul elements', function(){
+
+        it('should append 3 elements to the "root" element', function(done){
+            var result = '<section id="root"><p></p><p></p><p></p></section>';
+
+            jsdom.env('<section id="root"></section>', function(error, window){
+                var doc         = window.document,
+                    root        = doc.getElementById('root'),
+                    elements    = [
+                        doc.createElement('p'), 
+                        doc.createElement('p'), 
+                        doc.createElement('p')
+                    ];
+
+                utils.appendMultipleChildElements(root, elements);
+
+                assert.equal(result, root.outerHTML);
+                done();
+            });
+
+        });
+
+    });
+
+    describe('append an NodeList with 3 elements', function(){
+
+        it('should append 3 elements to the "root" element', function(done){
+            var result = '<section id="root"><p></p><p></p><p></p></section>';
+
+            jsdom.env('<section id="root"></section>', function(error, window){
+                var doc         = window.document,
+                    root        = doc.getElementById('root');
+
+                doc.body.appendChild(doc.createElement('p'));
+                doc.body.appendChild(doc.createElement('p'));
+                doc.body.appendChild(doc.createElement('p'));
+
+                utils.appendMultipleChildElements(root, doc.getElementsByTagName('p'));
+
+                assert.equal(result, root.outerHTML);
+                done();
+            });
+
+        });
+
+    });
+
+    describe('function is chainable', function(){
+
+        it('should return "this"', function(done){
+
+            jsdom.env('<section id="root"></section>', function(error, window){
+                var doc         = window.document,
+                    root        = doc.getElementById('root'),
+                    elements    = [];
+
+                var result = utils.appendMultipleChildElements(root, elements);
+
+                assert.equal(result, utils);
+                done();
+            });
+
+        });
+
+    });
+
+});
+
+
+
+describe('dom.utils.addFirstChildElement()', function(){
+    
+    describe('append a element as first child', function(){
+
+        it('should be the first child', function(done){
+            var result = '<section id="root"><p></p><div></div><div></div></section>';
+
+            jsdom.env('<section id="root"><div></div><div></div></section>', function(error, window){
+                var doc         = window.document,
+                    root        = doc.getElementById('root'),
+                    elP         = doc.createElement('p');
+
+                utils.addFirstChildElement(root, elP);
+
+                assert.equal(result, root.outerHTML);
+                done();
+            });
+
+        });
+
+    });
+
+    describe('function is chainable', function(){
+
+        it('should return "this"', function(done){
+
+            jsdom.env('<section id="root"></section>', function(error, window){
+                var doc         = window.document,
+                    root        = doc.getElementById('root'),
+                    elP         = doc.createElement('p');
+
+                var result = utils.addFirstChildElement(root, elP);
+
+                assert.equal(result, utils);
+                done();
+            });
+
+        });
+
+    });
+
+});
+
+
+
+describe('dom.utils.removeLastChildElement()', function(){
+    
+    describe('remove the last child element', function(){
+
+        it('should remove the last child', function(done){
+            var result = '<section id="root"><div></div><div></div></section>';
+
+            jsdom.env('<section id="root"><div></div><div></div><p></p></section>', function(error, window){
+                var root = window.document.getElementById('root');
+                utils.removeLastChildElement(root);
+
+                assert.equal(result, root.outerHTML);
+                done();
+            });
+
+        });
+
+    });
+
+    describe('root has no child elements', function(){
+
+        it('should keep the root untouched', function(done){
+            var result = '<section id="root"></section>';
+
+            jsdom.env('<section id="root"></section>', function(error, window){
+                var root = window.document.getElementById('root');
+                utils.removeLastChildElement(root);
+
+                assert.equal(result, root.outerHTML);
+                done();
+            });
+
+        });
+
+    });
+
+    describe('function is chainable', function(){
+
+        it('should return "this"', function(done){
+
+            jsdom.env('<section id="root"><div></div><div></div><p></p></section>', function(error, window){
+                var root = window.document.getElementById('root');
+                var result = utils.removeLastChildElement(root);
+
+                assert.equal(result, utils);
+                done();
+            });
+
+        });
+
+    });
+
+});
+
+
+
+describe('dom.utils.addSiblingAboveElement()', function(){
+    
+    describe('append a element above existing element', function(){
+
+        it('should append above', function(done){
+            var result = '<section id="root"><div></div><p></p><div id="foo"></div></section>';
+
+            jsdom.env('<section id="root"><div></div><div id="foo"></div></section>', function(error, window){
+                var doc         = window.document,
+                    root        = doc.getElementById('root'),
+                    element     = doc.getElementById('foo'),
+                    elP         = doc.createElement('p');
+
+                utils.addSiblingAboveElement(element, elP);
+
+                assert.equal(result, root.outerHTML);
+                done();
+            });
+            
+        });
+
+    });
+
+    describe('function is chainable', function(){
+
+        it('should return "this"', function(done){
+
+            jsdom.env('<section id="root"><div></div><div id="foo"></div></section>', function(error, window){
+                var doc         = window.document,
+                    root        = doc.getElementById('root'),
+                    element     = doc.getElementById('foo'),
+                    elP         = doc.createElement('p');
+
+                var result = utils.addSiblingAboveElement(element, elP);
+
+                assert.equal(result, utils);
+                done();
+            });
+
+        });
+
+    });
+
+});
+
+
+
+describe('dom.utils.addSiblingBelowElement()', function(){
+    
+    describe('append a element below existing element', function(){
+
+        it('should append below', function(done){
+            var result = '<section id="root"><div id="foo"></div><p></p><div></div></section>';
+
+            jsdom.env('<section id="root"><div id="foo"></div><div></div></section>', function(error, window){
+                var doc         = window.document,
+                    root        = doc.getElementById('root'),
+                    element     = doc.getElementById('foo'),
+                    elP         = doc.createElement('p');
+
+                utils.addSiblingBelowElement(element, elP);
+
+                assert.equal(result, root.outerHTML);
+                done();
+            });
+            
+        });
+
+    });
+
+    describe('function is chainable', function(){
+
+        it('should return "this"', function(done){
+
+            jsdom.env('<section id="root"><div id="foo"></div><div></div></section>', function(error, window){
+                var doc         = window.document,
+                    root        = doc.getElementById('root'),
+                    element     = doc.getElementById('foo'),
+                    elP         = doc.createElement('p');
+
+                var result = utils.addSiblingBelowElement(element, elP);
+
+                assert.equal(result, utils);
+                done();
+            });
+
+        });
+
+    });
+
+});
+
+
+
+describe('dom.utils.replaceElement()', function(){
+    
+    describe('replace an element with a new element', function(){
+
+        it('should replace the whole element and child elements', function(done){
+            var result = '<section id="root"><p></p></section>';
+
+            jsdom.env('<section id="root"><div></div><div></div></section>', function(error, window){
+                var doc         = window.document,
+                    root        = doc.getElementById('root'),
+                    element     = doc.createElement('section');
+
+                element.setAttribute('id', 'root');
+                element.appendChild(doc.createElement('p'));
+
+                utils.replaceElement(root, element);
+
+                assert.equal(result, doc.getElementById('root').outerHTML);
+                done();
+            });
+            
+        });
+
+    });
+
+    describe('returns an element', function(){
+
+        it('should return the new element', function(done){
+
+            jsdom.env('<section id="root"><div></div><div></div></section>', function(error, window){
+                var doc         = window.document,
+                    root        = doc.getElementById('root'),
+                    element     = doc.createElement('section');
+
+                element.setAttribute('id', 'root');
+                element.appendChild(doc.createElement('p'));
+
+                var result = utils.replaceElement(root, element);
+
+                assert.equal(result.outerHTML, element.outerHTML);
+                done();
+            });
+        });
+
+    });
+
+});
+
+
+
+describe('dom.utils.removeElement()', function(){
+    
+    describe('remove a given element', function(){
+
+        it('should leave the structure without the removed element', function(done){
+            var result = '<section id="root"><div></div><div></div></section>';
+
+            jsdom.env('<section id="root"><div></div><p id="foo"></p><div></div></section>', function(error, window){
+                var doc         = window.document,
+                    root        = doc.getElementById('root'),
+                    element     = doc.getElementById('foo');
+
+                utils.removeElement(element);
+
+                assert.equal(result, root.outerHTML);
+                done();
+            });
+            
+        });
+
+    });
+
+    describe('function is chainable', function(){
+
+        it('should return "this"', function(done){
+
+            jsdom.env('<section id="root"><p id="foo"></p></section>', function(error, window){
+                var doc         = window.document,
+                    root        = doc.getElementById('root'),
+                    element     = doc.getElementById('foo');
+
+                var result = utils.removeElement(element);
+
+                assert.equal(result, utils);
+                done();
+            });
+
+        });
+
+    });
+
+});
